04d523fa0bf0646e2402481c9a550ca2ca135d1a
BuildTools <cephas8080@outlook.com>
2021-11-28 18:04:26 -0100

    added test

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('test/index.html')
+@app.route('/test')
+def test():
+    return "test"
 
 if __name__ == '__main__':
     app.run()
