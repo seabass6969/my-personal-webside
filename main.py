42cc50c09d883f2acd69e229ec2095e2cd5384f2
BuildTools <cephas8080@outlook.com>
2021-12-12 11:02:40 -0100

    just finished the About page and the Project page

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projectname = ["my webside", "google"]
+projecturl = ["https://heroku.com", "https://google.com"]
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
-@app.route('/test')
-def test():
-    return "test"
+
+@app.route('/project')
+def project():
+    return render_template('project.html', Projectname=projectname, Projecturl=projecturl, longest=len(projectname))
+
+@app.route('/about')
+def blog():
+    return render_template('about.html')
+# @app.route('/test')
+# def test():
+#     return "test"
 
 if __name__ == '__main__':
     app.run(debug=False)
